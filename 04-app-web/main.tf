47b31c7e897259c204b035b8ee7bf9e47db53f22
khoavti <khoa.hoangdang@vti.com.vn>
2025-05-09 14:42:10 +0700

    update

--- 04-app-web/main.tf
+++ 04-app-web/main.tf
@@ -1,1 +1,15 @@
-// 04-app-web - main.tf
+# File: layers/04-app-web/main.tf
+
+terraform {
+  backend "local" {}  # Cấu hình backend local cho layer này
+}
+
+module "eventbridge_scheduler" {
+  source = "../modules/eventbridge-schedule"
+
+  environment        = var.environment
+  aws_region         = var.aws_region
+  aws_account_id     = var.aws_account_id
+  scheduler_role_arn = var.eventbridge_scheduler_role_arn
+  schedules          = var.schedules
+}
